e9dcaeb8061aa879d30723b37f2098b4a9edc7cc
Stephen Crowe <stephencrowe@Stephens-MacBook-Pro.local>
2026-02-10 13:49:12 +0100

    Initial Astro + Sanity commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+import netlify from '@astrojs/netlify'; // We will add this for deployment
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'hybrid', // Required for the Studio to work on a route
+  adapter: netlify(), 
+  integrations: [
+    react(),
+    sanity({
+      projectId: 'ots6ygg8', // Replace with your ID
+      dataset: 'production',
+      studioBasePath: '/admin', // The route where Studio will live
+    }),
+  ],
+});
