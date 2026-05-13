4fde6021c6cfdd4b91c2512e4052c85f07d0b374
invisibledot <stephen.edward.crowe@gmail.com>
2026-05-13 18:57:28 +0200

    Added integrations and navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
