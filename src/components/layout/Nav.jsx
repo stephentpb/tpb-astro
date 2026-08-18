03a08d9359ba6637f20bfcd4295c7ac7127e4137
invisibledot <stephen.edward.crowe@gmail.com>
2026-08-18 18:04:33 +0200

    New fonts, colors, images

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -11,7 +11,7 @@ export default function Nav() {
   ];
 
   return (
-    <nav className="bg-orange-300 shadow-md sticky top-0 z-50">
+    <nav className="bg-color-primary shadow-md sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         
         {/* Single Navigation Bar */}
