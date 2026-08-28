771c88570029a85c20c3f16b05d1b3b883b2894e
invisibledot <stephen.edward.crowe@gmail.com>
2026-08-28 17:16:16 +0200

    Trying new thing on donate page

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -11,17 +11,17 @@ export interface Testimonial {
 export const testimonials: Testimonial[] = [
   {
     id: 1,
-    quote: "We’re so fortunate to have the Third Place Books Literary Foundation in our community. Nothing brings joy like getting brand new books into the hands of aspiring readers, and the foundation makes this possible. We’re grateful for a donation of over 300 bright and engaging books that are now being read, reread, and loved by Team Read participants and their families.",
-    author: "Team Read",
-    role: "Local community organization",
-    image: "/images/testimonials/team-read.jpg",
-  },
-    {
-    id: 2,
     quote: "I’ve been lucky enough to watch Third Place Books work its magic first-hand. Trust me: There’s nothing quite like the moment a child hugs a new book to her heart like the friend she didn’t know she needed. Every time it happens, it’s a little miracle.",
     author: "Katherine Applegate",
     role: "Author",
     image: "/images/testimonials/katherine-applegate.jpg",
+  },
+    {
+    id: 2,
+    quote: "We’re so fortunate to have the Third Place Books Literary Foundation in our community. Nothing brings joy like getting brand new books into the hands of aspiring readers, and the foundation makes this possible. We’re grateful for a donation of over 300 bright and engaging books that are now being read, reread, and loved by Team Read participants and their families.",
+    author: "Team Read",
+    role: "Local community organization",
+    image: "/images/testimonials/team-read.jpg",
   },
 ];
 
