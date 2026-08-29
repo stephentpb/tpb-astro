1e9aaa19dc9ae05bdc9cfe94df92a7c174d5c3ca
invisibledot <stephen.edward.crowe@gmail.com>
2026-08-29 11:05:15 +0200

    New donate link on about page and fixed contact links and added endorsement

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -16,8 +16,15 @@ export const testimonials: Testimonial[] = [
     role: "Author",
     image: "/images/testimonials/katherine-applegate.jpg",
   },
-    {
+  {
     id: 2,
+    quote: "Gifting books to children enhances their self-confidence, empathy and imagination. It is so joyful to see a child, their eyes bright, hugging a book, and realizing “This book is mine!” Third Place Books Literary Foundation, thank you for impacting lives, inspiring dreams, and supporting literacy within our community.",
+    author: "Jewell Parker Rhodes",
+    role: "Author",
+    image: "/images/testimonials/jewell-parker-rhodes.jpg",
+  },
+  {
+    id: 3,
     quote: "We’re so fortunate to have the Third Place Books Literary Foundation in our community. Nothing brings joy like getting brand new books into the hands of aspiring readers, and the foundation makes this possible. We’re grateful for a donation of over 300 bright and engaging books that are now being read, reread, and loved by Team Read participants and their families.",
     author: "Team Read",
     role: "Local community organization",
